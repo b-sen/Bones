dcaf4e3033738dab1a53abfc4373ef3a0bb6cc1e
Bidisha Sen <794427+b-sen@users.noreply.github.com>
2021-05-17 01:09:35 -0400

    Add Scene Change Functionality
    Buttons to transition between scenes now work.

---
+++ Bones/Assets/Scripts/SceneNavigationScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void GoToGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
